5ab1112d50c995fe110f10a40c0abd7561c1edbf
Paulo <realjvuice@protonmail.com>
2020-09-29 15:45:19 -0300

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,6 @@ module.exports = {
     category: "Information",
 
     execute(client, message) {
-    	// message.channel.send('> `!file {Challenge_Number}`\n> `!hint {Challenge_Number}`\n> `!flag {Flag}` _Flag Format: cc{}_\n> `!source`\n> ⠀⠀⠀⠀⠀⠀⠀⠀⠀\n> **Challenges availables:** 1');
 	const embed = new Discord.MessageEmbed()
 		.setColor('#a632a8')
 	  	.setTitle('General')
