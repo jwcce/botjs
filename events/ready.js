0daa904b525c38bb8d302a4068665c17fd85a98d
Paulo <dbcloudserver@gmail.com>
2020-10-06 11:57:36 -0300

    Delete ready.js

--- events/ready.js
+++
@@ -1,7 +0,0 @@
-module.exports = (client) => {
-	console.log(`[~] Running with ${client.user.tag}`);
-	client.user.setActivity('!help', {
-		type: 'STREAMING',
-		url: 'https://twitch.tv/themayor11'
-	});
-}
