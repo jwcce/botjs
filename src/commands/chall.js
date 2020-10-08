abba7d43db978b8216ff68ed7e47957a48a53ab1
Paulo <realjvuice@protonmail.com>
2020-10-08 11:58:01 -0300

    Add files via upload

--- src/commands/chall.js
+++ src/commands/chall.js
@@ -1,7 +1,9 @@
-const { prefix } 		= require('../storage/config/config.json');
-const Discord		 	= require('discord.js');
+const { prefix } 	= require('../storage/config/config.json');
+const text		= require('../storage/config/text.json');	
+const Discord		= require('discord.js');
+const fs 		= require('fs');
 
-module.exports 			= {
+module.exports = {
     name: 'chall',
     aliases: ["challenge"],
     description: "Send challenge info",
@@ -11,28 +13,24 @@ module.exports 			= {
         if (!message.content.startsWith(prefix) || message.author.bot) return;
 
         if (!args.length) {
-		const embed 	= new Discord.MessageEmbed()
+		const embed = new Discord.MessageEmbed()
 			.setTitle('Challenges')
 			.setColor('#a632a8')
 			.setDescription('**3**')
 	
 		message.channel.send(embed);
-	} else if (args[0] >= 1) {
-		const file = args[0];
-        	const attach = new Discord.MessageAttachment(`./src/storage/file/${file}.zip`);
-		switch (args[0]) {
-			case '1':
-				message.channel.send('These images seem too heavy...', attach);
-				break;
-			case '2':
-				message.channel.send('Open your eyes!', attach);
-				break;
-			case '3':
-				message.channel.send('OHHH, im blind!', attach);
-				break;
-		}
-        } else {
-        	message.channel.send('File not found.')
-        }
+	} 
+	
+	fs.readdir('./src/storage/file', (err, files) => {
+		const max = files.length;
+		if (max > args[0] >= 1) {
+			const file = args[0];
+        		const attach = new Discord.MessageAttachment(`./src/storage/file/${file}.zip`);
+	
+			message.channel.send(text[file], attach);
+		} else {
+        		message.channel.send('Challenge not found.');
+       		}
+	});
     },
 };
